3a432abbc266117d6f20a4de337b7a03cce6b6a5
Jeremy Manning <jeremy.r.manning@dartmouth.edu>
2026-01-12 19:59:05 -0500

    fix: Health Connect permission request flow
    - Add HealthConnectPermissionDelegate.setPermissionDelegate() in MainActivity
    - Add PermissionsRationaleActivity for privacy policy display
    - Add activity-alias for Android 14+ VIEW_PERMISSION_USAGE
    - Add health permissions declarations to AndroidManifest
    - Add com.google.android.apps.healthdata to package queries
    
    Request Permissions button now opens Health Connect permission dialog.

---
+++ android/app/src/main/java/com/dreamstream/app/PermissionsRationaleActivity.kt
@@ -0,0 +1,24 @@
+package com.dreamstream.app
+
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class PermissionsRationaleActivity: AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    val webView = WebView(this)
+    webView.webViewClient = object : WebViewClient() {
+      override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+        return false
+      }
+    }
+
+    webView.loadUrl("https://context-lab.com/dream-stream/privacy")
+
+    setContentView(webView)
+  }
+}
